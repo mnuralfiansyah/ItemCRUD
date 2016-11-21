aaf046d06e01729911b5dd59c486e0b4f644001f
Ginanjar <egin066@gmail.com>
2016-11-21 07:25:41 +0700

    change layout

--- resources/views/ItemCRUD/create.blade.php
+++ resources/views/ItemCRUD/create.blade.php
@@ -1,51 +1,55 @@
-@extends('layouts.default')
+@extends('layouts.app')
 
 @section('content')
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Create New Item</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('itemCRUD.index') }}"> Back</a>
-            </div>
-        </div>
-    </div>
+<div class="container">
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+  <div class="row">
+      <div class="col-lg-12 margin-tb">
+          <div class="pull-left">
+              <h2>Create New Item</h2>
+          </div>
+          <div class="pull-right">
+              <a class="btn btn-primary" href="{{ route('itemCRUD.index') }}"> Back</a>
+          </div>
+      </div>
+  </div>
 
-    {!! Form::open(array('route' => 'itemCRUD.store','method'=>'POST')) !!}
-    <div class="row">
+  @if (count($errors) > 0)
+      <div class="alert alert-danger">
+          <strong>Whoops!</strong> There were some problems with your input.<br><br>
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+  @endif
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
-            </div>
-        </div>
+  {!! Form::open(array('route' => 'itemCRUD.store','method'=>'POST')) !!}
+  <div class="row">
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                {!! Form::textarea('description', null, array('placeholder' => 'Description','class' => 'form-control','style'=>'height:100px')) !!}
-            </div>
-        </div>
+      <div class="col-xs-12 col-sm-12 col-md-12">
+          <div class="form-group">
+              <strong>Title:</strong>
+              {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
+          </div>
+      </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+      <div class="col-xs-12 col-sm-12 col-md-12">
+          <div class="form-group">
+              <strong>Description:</strong>
+              {!! Form::textarea('description', null, array('placeholder' => 'Description','class' => 'form-control','style'=>'height:100px')) !!}
+          </div>
+      </div>
 
-    </div>
-    {!! Form::close() !!}
+      <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+
+  </div>
+  {!! Form::close() !!}
+
+</div>
 
 @endsection
